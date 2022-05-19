1b4af2ac6259a2296f76dfc37ae62f6d9b5ff9ad
kike-pinto <cripintort@gmail.com>
2022-05-18 23:51:43 -0400

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo illo,
+            perferendis odit consequatur inventore accusantium voluptate
+            repellendus sunt et nam repellat velit excepturi, non corrupti
+            molestiae earum hic recusandae totam dicta repudiandae suscipit
+            obcaecati, fugit voluptatum! Quam, illum, cum eveniet tempore ut
+            facere officia nam laboriosam excepturi, eius velit facilis.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
